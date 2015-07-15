b21a1d4bbe40afc0bd0217450ed42e1d21421219
Scott Taylor <scott.c.taylor@mac.com>
2015-07-15 14:00:32 -0400

    Make grid images appear in their original orientation

--- image-flow.php
+++ image-flow.php
@@ -6,3 +6,10 @@ Description: A new user experience for the Media Flow in WordpPress.
 Version: 0.1
 Author: The Image Flow Team
 */
+
+// __DIR__ is only available in later versions of PHP
+$dir = dirname( __FILE__ );
+
+require_once( $dir . '/php/class-image-flow-plugin.php' );
+
+add_action( 'plugins_loaded', array( 'Image_Flow_Plugin', 'get_instance' ) );
---
+++ php/class-image-flow-plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+class Image_Flow_Plugin {
+	static $instance;
+
+	public static function get_instance() {
+		// static:: is only available in later versions of PHP
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct() {
+		// actions and filters
+		add_action( 'wp_enqueue_media', array( $this, 'wp_enqueue_media' ) );
+	}
+
+	public function wp_enqueue_media() {
+		$dir = dirname( __FILE__ );
+		wp_enqueue_style( 'image-flow-css', plugins_url( 'css/image-flow.css', $dir ) );
+	}
+}
